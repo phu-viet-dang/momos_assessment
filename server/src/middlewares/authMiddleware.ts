bb6a9c7b4c82cec03ac1c80a086490a8725b04dc
phu-viet-dang <phuvietdang@gmail.com>
2024-11-24 23:45:05 +0700

    fix: auth middleware allow null string token

--- server/src/middlewares/authMiddleware.ts
+++ server/src/middlewares/authMiddleware.ts
@@ -3,17 +3,23 @@ import UserModel from "../sequelize/models/user.model";
 
 const authMiddleware = async (req: any, res: any, next: any) => {
   try {
-    const token = req.header("Authorization")?.replace("Bearer ", "");
+    const token = req.header("Authorization")?.split(" ")[1];
+
+    if (!!!token || token == "null") {
+      return res.status(403).json({ error: "Unauthorized request!" });
+    }
+
     const data: any = jsonwebtoken.verify(token, process.env.JWT_SECRET);
     const user = await UserModel.findOne({
       where: { username: data.username },
     });
-    if (user) {
-      req.user = user.transformToResponse();
-      next();
-      return;
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found!" });
     }
-    return res.status(403).json({ error: "Unauthorized" });
+
+    req.user = user.transformToResponse();
+    next();
   } catch (error) {
     next(error);
   }
